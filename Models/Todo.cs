7eb1f2f05752323401b60a72885dc6ea243bd978
Artur Oliveira <arturolivv@gmail.com>
2024-09-18 18:50:08 -0300

    first commit

---
+++ Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public bool Done { get; set; }
+    }
+}
